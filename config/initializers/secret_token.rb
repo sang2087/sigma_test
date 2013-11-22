39384c7d3b937b96bf978aad55093a6fda62a888
hogoo <sang2087@gmail.com>
2013-11-22 13:05:46 +0900

    sigma setting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SigmaProject::Application.config.secret_token = 'bf46775a1eb7df7686c1ba14cbbcebd70daf45dc5b47a555390576319898cc2fd2edb88d8a605670522508e8fa34f3efda34f2dc0dc0d9b1082facbdeb074449'
